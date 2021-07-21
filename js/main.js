ed26a586e0f41bb0f605e37370f1ae9a2d08ffa3
Pietro <pietrospitella@gmail.com>
2021-07-21 18:03:57 +0200

    minor bug resolved

--- js/main.js
+++ js/main.js
@@ -65,14 +65,18 @@
 
 //  // Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro
 
-// for (var i = 0; i < array_1.length; i++) {
 
-//   if (array_1.length < array_2.length ) {
-//     array_1.push(array_1[i]);
-//   }
-//   else if (array_2.length < array_1.length ) {
-//     array_2.push(array_2[i]);
-//   }
+// var i = 0;
+
+
+// while (array_1.length != array_2.length) {
+    
+//     if (array_1.length < array_2.length ) {
+//         array_1.push(array_1[i]);
+//     }
+//     else if (array_2.length < array_1.length ) {
+//         array_2.push(array_2[i]);
+//     }
 // }
 
 // console.log(array_1, array_2);
